59820b11463f983cc98900de6c10346bca1997d2
Alexandr <alexandrmail32@gmail.com>
2019-04-29 19:00:40 +0300

    Solution

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
 'use strict';
+const card = document.querySelector('.wraper');
+const mainBlock = document.getElementsByTagName('section');
+for (let i = 0; i < 7; i++) {
+  const newCard = card.cloneNode(true);
+  mainBlock[0].appendChild(newCard);
+}
