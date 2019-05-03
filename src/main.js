d7385d27fc59d9acfe4e6bc89acaf38a9231ddf9
Yevhen-Kryvdiuk <krivachacha@gmail.com>
2019-05-03 17:58:14 +0300

    use template string

--- src/main.js
+++ src/main.js
@@ -1,53 +1,53 @@
 'use strict';
-const card = `<div class="card" data-qa="card">\n`
-  + `  <div class="image">\n`
-  + `    <img src="./images/imac.jpeg" alt="">\n`
-  + `  </div>\n`
-  + `  <div class="info">\n`
-  + `    <div class="title">\n`
-  + `      <h3>Моноблок APPLE A1419 iMac 27" Retina 5K (MNED2UA/A)</h3>\n`
-  + `    </div>\n`
-  + `    <div class="product-code">\n`
-  + `      <p>Код товара: 195434</p>\n`
-  + `    </div>\n`
-  + `    <div class="reviews">\n`
-  + `      <div id="reviewStars-input">\n`
-  + `        <input id="star-0" type="radio" class="stars">\n`
-  + `        <label for="star-0">\n`
-  + `          <img src="images/star-active.svg" alt="star">\n`
-  + `        </label>\n`
-  + `        <input id="star-1" type="radio" class="stars">\n`
-  + `        <label for="star-1">\n`
-  + `          <img src="images/star-active.svg" alt="star">\n`
-  + `        </label>\n`
-  + `        <input id="star-2" type="radio" class="stars">\n`
-  + `        <label for="star-2">\n`
-  + `          <img src="images/star-active.svg" alt="star">\n`
-  + `        </label>\n`
-  + `        <input id="star-3" type="radio" class="stars" checked>\n`
-  + `        <label for="star-3">\n`
-  + `          <img src="images/star-active.svg" alt="star">\n`
-  + `        </label>\n`
-  + `        <input id="star-4" type="radio" class="stars">\n`
-  + `        <label for="star-4">\n`
-  + `          <img src="images/star.svg" alt="star">\n`
-  + `        </label>\n`
-  + `      </div>\n`
-  + `      <div class="number-of-reviews">\n`
-  + `        <p>Отзывов: 5</p>\n`
-  + `      </div>\n`
-  + `    </div>\n`
-  + `    <div class="price">\n`
-  + `      <p>Цена: </p>\n`
-  + `      <div class="cost">\n`
-  + `        <p>69 999 грн</p>\n`
-  + `      </div>\n`
-  + `    </div>\n`
-  + `    <div class="buy-button">\n`
-  + `      <a href="#" data-qa="hover">Купить</a>\n`
-  + `    </div>\n`
-  + `  </div>\n`
-  + `</div>`;
+const card = `<div class="card" data-qa="card">
+    <div class="image">
+      <img src="./images/imac.jpeg" alt="">
+    </div>
+    <div class="info">
+      <div class="title">
+        <h3>Моноблок APPLE A1419 iMac 27" Retina 5K (MNED2UA/A)</h3>
+      </div>
+      <div class="product-code">
+        <p>Код товара: 195434</p>
+      </div>
+      <div class="reviews">
+        <div id="reviewStars-input">
+          <input id="star-0" type="radio" class="stars">
+          <label for="star-0">
+            <img src="images/star-active.svg" alt="star">
+          </label>
+          <input id="star-1" type="radio" class="stars">
+          <label for="star-1">
+            <img src="images/star-active.svg" alt="star">
+          </label>
+          <input id="star-2" type="radio" class="stars">
+          <label for="star-2">
+            <img src="images/star-active.svg" alt="star">
+          </label>
+          <input id="star-3" type="radio" class="stars" checked>
+          <label for="star-3">
+            <img src="images/star-active.svg" alt="star">
+          </label>
+          <input id="star-4" type="radio" class="stars">
+          <label for="star-4">
+            <img src="images/star.svg" alt="star">
+          </label>
+        </div>
+        <div class="number-of-reviews">
+          <p>Отзывов: 5</p>
+        </div>
+      </div>
+      <div class="price">
+        <p>Цена: </p>
+        <div class="cost">
+          <p>69 999 грн</p>
+        </div>
+      </div>
+      <div class="buy-button">
+        <a href="#" data-qa="hover">Купить</a>
+      </div>
+    </div>
+  </div>`;
 
 let htmlElements = '';
 for (let i = 0; i < 8; i++) {
