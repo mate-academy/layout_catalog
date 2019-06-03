7c16820e1e2df0b075e883bff513024835569bd8
RavenVol <r.volianiuk@gmail.com>
2019-06-03 03:43:56 +0300

    Partial Solved HW9 Catalo. Most test failed. I don't caregit status! 30.05.2019 Raven

---
+++ src/menu.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var menu = '\
+  <header class="siteMenu">\
+    <img class="menuLogo" src="./images/logo.png" alt="MOYO logo">\
+    <nav class="menuButtonContainer">\
+      <a class="menuButton is-active" href="#">APPLE</a>\
+      <a class="menuButton" href="#">SAMSUNG</a>\
+      <a class="menuButton" href="#">СМАРТФОНЫ И ТЕЛЕФОНЫ</a>\
+      <a class="menuButton" data-qa="hover" href="#">НОУТБУКИ И КОМПЬЮТЕРЫ</a>\
+      <a class="menuButton" href="#">ГАДЖЕТЫ</a>\
+      <a class="menuButton" href="#">ПЛАНШЕТЫ</a>\
+      <a class="menuButton" href="#">ФОТО</a>\
+      <a class="menuButton" href="#">ВИДЕО</a>\
+    </nav>\
+  </header>';
+
+document.write(menu);
